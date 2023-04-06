6ddae6fc625115bd22653262a267ffee24d1bfd5
Tiffany Lee <tl5110@g.rit.edu>
2023-04-05 21:00:48 -0400

    Implemented and tested Clock(), ClockConfig(), and Solver()

--- src/puzzles/clock/ClockConfig.java
+++ src/puzzles/clock/ClockConfig.java
@@ -2,31 +2,58 @@ package puzzles.clock;
 
 import puzzles.common.solver.Configuration;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class ClockConfig implements Configuration {
+    private final int hours;
+    private final int start;
+    private final int end;
+
+    public ClockConfig(int hours, int start, int end){
+        this.hours = hours;
+        this.start = start;
+        this.end = end;
+    }
     @Override
     public boolean isSolution() {
-        return false;
+        return start == end;
     }
 
     @Override
     public Collection<Configuration> getNeighbors() {
-        return null;
+        Collection<Configuration> neighborsList = new ArrayList<>();
+        int up = start + 1;
+        int down = start - 1;
+        if(up > hours){
+            up = 1;
+        } else if(down <= 0){
+            down = hours;
+        }
+        ClockConfig forward = new ClockConfig(hours, up, end);
+        ClockConfig backward = new ClockConfig(hours, down, end);
+        neighborsList.add(backward);
+        neighborsList.add(forward);
+        return neighborsList;
     }
 
     @Override
     public boolean equals(Object other) {
+        if(other instanceof ClockConfig otherClock){
+            return this.hours == otherClock.hours
+                    && this.start == otherClock.start
+                    && this.end == otherClock.end;
+        }
         return false;
     }
 
     @Override
     public int hashCode() {
-        return 0;
+        return this.hours + this.start + this.end;
     }
 
     @Override
     public String toString() {
-        return null;
+        return String.valueOf(start);
     }
 }
--- src/puzzles/common/solver/Solver.java
+++ src/puzzles/common/solver/Solver.java
@@ -1,5 +1,58 @@
 package puzzles.common.solver;
 
-public class Solver {
-    // TODO
+import java.util.*;
+
+
+public class Solver{
+    private final HashMap<Configuration, Configuration> predecessors = new HashMap<>();
+    private final Queue<Configuration> queue = new LinkedList<>();
+    private final Configuration start;
+    private int totalConfigs = 0;
+    private int uniqueConfigs = 0;
+
+    public Solver(Configuration start){
+        this.start = start;
+    }
+
+    public Collection<Configuration> solve(){
+        queue.add(start);
+        predecessors.put(start, start);
+        uniqueConfigs += queue.size();
+        totalConfigs += predecessors.size();
+        while(!queue.isEmpty()){
+            Configuration current = queue.remove();
+            if (current.isSolution()) {
+                return constructPath(predecessors, current);
+            } else {
+                Collection<Configuration> successors = current.getNeighbors();
+                totalConfigs += successors.size();
+                for (Configuration nbr : successors) {
+                    if (!predecessors.containsKey(nbr)) {
+                        uniqueConfigs += 1;
+                        predecessors.put(nbr, current);
+                        queue.add(nbr);
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    public Collection<Configuration> constructPath(HashMap<Configuration, Configuration> predecessors,
+            Configuration end){
+        List<Configuration> path = new LinkedList<>();
+        if(predecessors.containsKey(end)) {
+            while(end != start){
+                path.add(0, end);
+                end = predecessors.get(end);
+            }
+            path.add(0, start);
+        }
+        return path;
+    }
+
+    public int getTotalConfigs() { return totalConfigs; }
+
+    public int getUniqueConfigs() { return uniqueConfigs; }
 }
+
